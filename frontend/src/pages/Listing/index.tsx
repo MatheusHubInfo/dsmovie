9371a391301bb1d79efbe359bb25d0b2490cdd9e
Matheus Alves <mm5426875@gmail.com>
2022-01-16 23:23:27 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,15 @@
+import axios from "axios";
+import { BASE_URL } from "utils/requests";
 import MovieCard from "../../components/MovieCard";
 import Pagination from "../../components/Pagination";
 
 function Listing() {
+
+    //FORMA ERRADA
+    axios.get(`${BASE_URL}/movies/2`)
+    .then(response => {
+        console.log(response.data);
+    })
 
     return (
         <>
